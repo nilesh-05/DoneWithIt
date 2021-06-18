b807a543c90df7fdfe0c5aa24fc80fd3b6140b36
nilesh-05 <nilesh0411.cse19@chitkara.edu.in>
2021-06-18 16:52:50 +0530

    used switch, modal and made own Picker

--- App.js
+++ App.js
@@ -1,32 +1,33 @@
 import React, { useState } from "react";
-import { SafeAreaView, StyleSheet, View } from "react-native";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
 
-import { Text, TextInput } from "react-native";
+import Screen from "./app/components/Screen";
+import AppPicker from "./app/components/AppPicker";
 import AppTextInput from "./app/components/AppTextInput";
 
+const categories = [
+	{ label: "Furniture", value: 1 },
+	{ label: "Clothing", value: 2 },
+	{ label: "Cameras", value: 3 },
+];
+
 export default function App() {
-	const [firstName, setFirstName] = useState("");
+	const [category, setCategory] = useState(categories[0]);
+
 	return (
 		<Screen>
-			<AppTextInput placeholder="Username" icon="email" />
+			{/* <Switch
+				value={isNew}
+				onValueChange={(newValue) => setIsNew(newValue)}
+			/> */}
+
+			<AppPicker
+				selectedItem={category}
+				onSelectItem={(item) => setCategory(item)}
+				items={categories}
+				icon="apps"
+				placeholder="Category"
+			/>
+			<AppTextInput icon="email" placeholder="Email" />
 		</Screen>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-});
