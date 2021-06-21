5a769d05e1982e91e6401e9b0d16bf00d89ecabc
nilesh-05 <nilesh0411.cse19@chitkara.edu.in>
2021-06-21 16:04:28 +0530

    reorganizing forms folder

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -2,10 +2,8 @@ import React from "react";
 import { StyleSheet, Image } from "react-native";
 import * as Yup from "yup";
 
-import AppForm from "../components/AppForm";
-import AppFormField from "../components/AppFormField";
-import SubmitButton from "../components/SubmitButton";
 import Screen from "../components/Screen";
+import { AppForm, AppFormField, SubmitButton } from "../components/forms";
 
 const validationSchema = Yup.object().shape({
 	email: Yup.string().required().email().label("Email"),
