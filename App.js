0b2efe011d104e8e59c78b423fdc3652f354d49d
nilesh-05 <nilesh0411.cse19@chitkara.edu.in>
2021-06-24 14:12:31 +0530

    started refactoring

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from "react";
-import RegisterScreen from "./app/screens/RegisterScreen";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
-	return <RegisterScreen />;
+	// return <MessagesScreen />;
+	return <ListingEditScreen />;
 }
