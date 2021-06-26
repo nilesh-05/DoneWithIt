7e986cb3bdd1b4939ff64832e395b3da82e08cf0
nilesh-05 <nilesh0411.cse19@chitkara.edu.in>
2021-06-26 14:44:24 +0530

    building image selection functionality

--- App.js
+++ App.js
@@ -1,8 +1,41 @@
-import React from "react";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
+import React, { useEffect, useState } from "react";
+import * as ImagePicker from "expo-image-picker";
+import Screen from "./app/components/Screen";
+import Button from "./app/components/Button";
+import * as Permissions from "expo-permissions";
+import { Image } from "react-native";
+import ImageInput from "./app/components/ImageInput";
 
 export default function App() {
-	// return <MessagesScreen />;
-	return <ListingEditScreen />;
+	const [imageUri, setImageUri] = useState();
+
+	const requestPermission = async () => {
+		const { granted } =
+			await ImagePicker.requestMediaLibraryPermissionsAsync();
+		if (!granted)
+			alert("You need to enable permission to access the library");
+	};
+
+	useEffect(() => {
+		requestPermission();
+	}, []);
+
+	const selectImage = async () => {
+		try {
+			const result = await ImagePicker.launchImageLibraryAsync();
+			if (!result.cancelled) {
+				setImageUri(result.uri);
+			}
+		} catch (error) {
+			console.log("Error reading Image ", error);
+		}
+	};
+	return (
+		<Screen>
+			<ImageInput
+				onChangeImage={(uri) => setImageUri(uri)}
+				imageUri={imageUri}
+			/>
+		</Screen>
+	);
 }
