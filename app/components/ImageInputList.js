b8ac630616eb2ac53999f3004024eaae191d4f7d
nilesh-05 <nilesh0411.cse19@chitkara.edu.in>
2021-07-05 12:44:31 +0530

    added scrolling in image input list

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,18 +1,28 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useRef } from "react";
+import { ScrollView, StyleSheet, View } from "react-native";
 import ImageInput from "./ImageInput";
 
 function ImageInputList({ imageUris = [], onRemoveImage, onAddImage }) {
+	const scrollView = useRef();
 	return (
-		<View style={styles.container}>
-			{imageUris.map((uri) => (
-				<ImageInput
-					imageUri={uri}
-					key={uri}
-					onChangeImage={() => onRemoveImage(uri)}
-				/>
-			))}
-			<ImageInput onChangeImage={(uri) => onAddImage(uri)} />
+		<View>
+			<ScrollView
+				ref={scrollView}
+				horizontal
+				onContentSizeChange={() => scrollView.current.scrollToEnd()}
+			>
+				<View style={styles.container}>
+					{imageUris.map((uri) => (
+						<View key={uri} style={styles.image}>
+							<ImageInput
+								imageUri={uri}
+								onChangeImage={() => onRemoveImage(uri)}
+							/>
+						</View>
+					))}
+					<ImageInput onChangeImage={(uri) => onAddImage(uri)} />
+				</View>
+			</ScrollView>
 		</View>
 	);
 }
@@ -20,6 +30,10 @@ function ImageInputList({ imageUris = [], onRemoveImage, onAddImage }) {
 const styles = StyleSheet.create({
 	container: {
 		flexDirection: "row",
+	},
+	image: {
+		marginRight: 5,
+		marginLeft: 5,
 	},
 });
 
