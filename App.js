10f459e6ea166d1d76dadb551748592aeb79fe13
nilesh-05 <nilesh0411.cse19@chitkara.edu.in>
2021-07-08 14:51:32 +0530

    started navigation

--- App.js
+++ App.js
@@ -1,6 +1,33 @@
-import React, { useEffect, useState } from "react";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import React from "react";
+import { Text } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+import Screen from "./app/components/Screen";
+
+const Tweets = () => {
+	<Screen>
+		<Text>Tweets</Text>
+	</Screen>;
+};
+const TweetDetails = () => {
+	<Screen>
+		<Text>Tweet Details</Text>
+	</Screen>;
+};
+
+const Stack = createStackNavigator();
+const StackNavigator = () => {
+	<Stack.Navigator>
+		<Stack.Screen name="Tweets" component={Tweets} />
+		<Stack.Screen name="TweetDetails" component={TweetDetails} />
+	</Stack.Navigator>;
+};
 
 export default function App() {
-	return <ListingEditScreen />;
+	return (
+		<NavigationContainer>
+			<StackNavigator />
+		</NavigationContainer>
+	);
 }
