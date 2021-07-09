9db4586a79beb471c385d9fdf43a096250b3d019
nilesh-05 <nilesh0411.cse19@chitkara.edu.in>
2021-07-09 14:29:03 +0530

    made FeedNavigator with modal

---
+++ app/navigation/FeedNavigator.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+	createStackNavigator,
+	TransitionPresets,
+} from "@react-navigation/stack";
+import ListingsScreen from "../screens/ListingsScreen";
+import ListingDetailsScreen from "../screens/ListingDetailsScreen";
+
+const Stack = createStackNavigator();
+
+const FeedNavigator = () => (
+	<Stack.Navigator
+		mode="modal"
+		screenOptions={{ headerTitleAlign: "center", gestureEnabled: true }}
+	>
+		<Stack.Screen
+			name="Listings"
+			component={ListingsScreen}
+			options={{
+				...TransitionPresets.ScaleFromCenterAndroid,
+			}}
+		/>
+		<Stack.Screen
+			name="ListingDetails"
+			component={ListingDetailsScreen}
+			options={{
+				...TransitionPresets.RevealFromBottomAndroid,
+				headerShown: false,
+			}}
+		/>
+	</Stack.Navigator>
+);
+
+export default FeedNavigator;
